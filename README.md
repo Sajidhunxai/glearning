0c17557affb59c956243f9e22c13f37b2819c81e
sajid hunxai <89635016+Sajidhunxai@users.noreply.github.com>
2022-06-12 19:17:16 +0500

    first

---
+++ README.md
@@ -0,0 +1,19 @@
+# masterstudy_app
+
+Master Study 2.1.0
+
+New: The Flutter version is updated to 3.0.0 
+Fix: Minor bug fixes
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/ui/bloc/quiz_screen/quiz_screen_bloc.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+import 'package:bloc/bloc.dart';
+import 'package:inject/inject.dart';
+import 'package:masterstudy_app/data/repository/lesson_repository.dart';
+import './bloc.dart';
+
+@provide
+class QuizScreenBloc extends Bloc<QuizScreenEvent, QuizScreenState> {
+  final LessonRepository _repository;
+
+  QuizScreenState get initialState => InitialQuizScreenState();
+
+  QuizScreenBloc(this._repository) : super(InitialQuizScreenState()) {
+    on<FetchEvent>((event, emit) async {
+      try {
+        if (event.quizResponse != null) emit(LoadedQuizScreenState(event.quizResponse));
+        var response = await _repository.getLesson(event.courseId, event.lessonId);
+        emit(LoadedQuizScreenState(response));
+      } catch (e, s) {
+        print(e);
+        print(s);
+      }
+    });
+  }
+}
